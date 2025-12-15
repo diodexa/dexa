4ad3355ab1117faaa0f0941a758cc60e2c894edb
Dio <diodexa@gmail.com>
2025-12-15 16:07:14 +0700

    ganti file yang ga sesuai hurufnya
    image hover ganti i nya jadi huruf kecil

--- GrupChat/Script/Whatsapp_v2.js
+++ GrupChat/Script/Whatsapp_v2.js
@@ -1,11 +1,11 @@
 import { API_URL, sendMessage } from "./components/API.js";
 import { ButtonKirim } from "./components/ButtonKirim.js";
 import { clipboard } from "./components/ClipboardRek.js";
-import { imageHover } from "./components/ImageHover.js";
+import { imageHover } from "./components/imageHover.js";
 import { ButtonJoin } from "./components/JoinButton.js";
 import { KirimStiker } from "./components/KirimStiker.js";
 import { loadPesan } from "./components/LoadPesan.js";
-import { hideStickerOverlay, showStickerOverlay, TampilStiker } from "./components/modalStiker.js";
+import {  TampilStiker } from "./components/modalStiker.js";
 import { NamaTamu } from "./components/Paramps.js";
 import { AudioIcon } from "./components/PlayAudio.js";
 
