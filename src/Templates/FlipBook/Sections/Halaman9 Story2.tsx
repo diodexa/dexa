da7ba45421a2fbe66b02cee2ea1b72b9f3e32a01
Dio <diodexa@gmail.com>
2026-09-14 21:30:54 +0700

    update halaman9 story
    lg font size

--- src/Templates/FlipBook/Sections/Halaman9 Story2.tsx
+++ src/Templates/FlipBook/Sections/Halaman9 Story2.tsx
@@ -17,8 +17,8 @@ const Halaman9  = ({data}:Props) =>  {
               return (
                 <div key={index} className={`flex flex-row border p-1 w-full h-1/2 mb-1`}>
                   <div className="leading-none text-right w-2/4 pr-1">
-                      <h2 className="text-[0.8rem] lg:text-[0.6rem] mb-1"><strong>{Cerita.Head}</strong></h2>
-                      <p className="text-[0.6rem] lg:text-[0.5rem] whitespace-pre-line">{Cerita.Story}</p>
+                      <h2 className="text-[0.8rem] lg:text-[0.5rem] mb-1"><strong>{Cerita.Head}</strong></h2>
+                      <p className="text-[0.6rem] lg:text-[0.4rem] whitespace-pre-line">{Cerita.Story}</p>
                   </div>
                   <div className="relative w-2/4 ">
                       <img src={data.gallery?.[index % 2 === 0 ? 3 : 4]} alt="Foto Story" className="w-1/3  absolute inset-0 w-full h-full object-cover object-center" />
