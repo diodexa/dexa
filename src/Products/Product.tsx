abf80cfbeb7688fc86e3e4c8c1868e02a521b7b1
Dio <diodexa@gmail.com>
2026-09-21 12:00:50 +0700

    add product
    add product

--- src/Products/Product.tsx
+++ src/Products/Product.tsx
@@ -5,34 +5,42 @@ interface Project {
 }
 
 const projects: Project[] = [
-  { title: "Flip book", 
-    img: "/ProjectBook.webp", 
-    demoUrl: "https://dexa-invitation.com/Flipbook" },
-
-  { title: "Scroll Leaf", 
-    img: "/ProjectLeaf.webp", 
-    demoUrl: "https://dexa-invitation.com/ScrollLeaf" },
-
-  { title: "Scroll Cream", 
-    img: "/ProjectJourneyCream.webp", 
-    demoUrl: "https://dexa-invitation.com/ScrollCream" },
-
   
-  { title: "Group Chat", 
-    img: "/projectGroupChat.webp", 
-    demoUrl: "https://dexa-invitation.com/GroupChat" },
-    
-  { title: "Monochrome", 
-    img: "/ProjectMonochrome.webp", 
-    demoUrl: "https://dexa-invitation.com/Monochrome" },
+  { title: "Pink Green", 
+    img: "/PinkGreen.webp", 
+    demoUrl: "https://dexa-invitation.com/PinkGreen" },
+
+  { title: "Rustic", 
+    img: "/ProjectRustic.webp", 
+    demoUrl: "https://dexa-invitation.com/Rustic" },
 
   { title: "Sunflower", 
     img: "/ProjectSunflower.webp", 
     demoUrl: "https://dexa-invitation.com/Sunflower" },
 
-  { title: "Rustic", 
-    img: "/ProjectRustic.webp", 
-    demoUrl: "https://dexa-invitation.com/Rustic" },
+    
+  { title: "Scroll Leaf", 
+    img: "/ProjectLeaf.webp", 
+  demoUrl: "https://dexa-invitation.com/ScrollLeaf" },
+  
+  { title: "Scroll Cream", 
+    img: "/ProjectJourneyCream.webp", 
+  demoUrl: "https://dexa-invitation.com/ScrollCream" },
+  
+  
+  { title: "Group Chat", 
+    img: "/projectGroupChat.webp", 
+    demoUrl: "https://dexa-invitation.com/GroupChat" },
+
+  { title: "Flip book", 
+    img: "/ProjectBook.webp", 
+    demoUrl: "https://dexa-invitation.com/Flipbook" },
+    
+  { title: "Monochrome", 
+    img: "/ProjectMonochrome.webp", 
+    demoUrl: "https://dexa-invitation.com/Monochrome" },
+
+
 ];
 
 export default projects;
--- src/Templates/Pink/Sections/1.Opening.tsx
+++ src/Templates/Pink/Sections/1.Opening.tsx
@@ -14,7 +14,7 @@ const Opening = ({ data, isOpen }: Props) => {
     style={{ color: data.theme?.contrasfont }}>
       <div className=" flex z-2 mt-15 ml-10 flex-col max-w-1/2  gap-5">
             <p className="text-l uppercase tracking-[0.27em]">The Weeding Of </p>
-            <h2 className={`font-BylinerScript  ${(data.NamagroomPanggilan.length)>=7 ? "text-5xl leading-15" : "text-6xl leading-20" }`}>{data.NamabridePanggilan} & {data.NamagroomPanggilan}</h2>
+            <h2 className={`font-BylinerScript lg:text-4xl lg:leading-15  ${(data.NamagroomPanggilan.length || data.NamabridePanggilan.length)>=7 ? "text-5xl leading-15 " : "text-6xl leading-20" }`}>{data.NamabridePanggilan} & {data.NamagroomPanggilan}</h2>
         </div>
     </section>
         
